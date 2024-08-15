2f44168c5b57df8c3490f007e544ba19a2fab6a7
iana27 <ianocika2787@yahoo.com>
2024-08-15 12:01:20 -0400

    Iana's first commit

---
+++ src/letsplay/TestCaseIana.java
@@ -0,0 +1,16 @@
+package letsplay;
+
+public class TestCaseIana {
+
+	public static void main(String[] args) {
+	boolean lifeIsBeautiful=true;
+	
+	if(lifeIsBeautiful) {
+		String enjoy="Enjoy, make kids, have fun";
+	}else {
+		String noElse="Life is Beautiful.Enjoy, make kids, have fun!Period";
+	}
+
+	}
+
+}
